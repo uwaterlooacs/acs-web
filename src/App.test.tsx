57d0b6d2167bd9d066d677d2544380b9b5dfa98b
Joshua Allum <joshuatallum@gmail.com>
2020-03-13 13:01:14 -0400

    Travis CI automatic deployments (#13)
    * Add firebase deploy and travis.yml
    
    * Fix linting errors
    
    * Add done call in async useEvents test
    
    * Update App test to not be async
    
    * Add deployment info to CI yml
    
    * Add done call to App test
    
    * Remove the damn App test
    
    * Remove the next damn test
    
    * Add passWithNoTests flag

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { waitForElement } from '@testing-library/react';
-import { render } from 'utils/test';
-import App from './App';
-
-test('renders learn react link', async () => {
-  const { getByText } = render(<App />);
-  await waitForElement(() => getByText(/Home/i));
-});
