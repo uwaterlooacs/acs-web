d564e11bd5592fa73275b41a7b2e6e20e5db16db
Christopher Mohammed <christophermohammed11@outlook.com>
2020-03-13 12:54:13 -0400

    made generic hook for data stored in firebase

---
+++ src/modules/data/useData.ts
@@ -0,0 +1,18 @@
+import { useFirestore, useFirestoreCollectionData } from 'reactfire';
+
+const useData = <T, D>(COLLECTION_NAME: string) => {
+  const dataRef = useFirestore().collection(COLLECTION_NAME);
+  const data = useFirestoreCollectionData<T>(dataRef, {
+    idField: 'id',
+  });
+  const addData = async (d: D) => {
+    const creationDate = new Date();
+    return await dataRef.add({ ...d, creationDate });
+  };
+  const removeData = async (id: string) => {
+    return await dataRef.doc(id).delete();
+  };
+  return { data, addData, removeData };
+};
+
+export default useData;
