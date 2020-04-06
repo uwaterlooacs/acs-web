169f7559c553ebb7e351bf31963c5ee5f1270c14
Joshua Allum <joshuatallum@gmail.com>
2020-04-06 18:31:43 -0400

    Use css helper for syntax highlighting

--- src/components/utils/Input/Input.ts
+++ src/components/utils/Input/Input.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components/macro';
+import styled, { css } from 'styled-components/macro';
 import {
   space,
   layout,
@@ -11,16 +11,16 @@ import {
 import { InputProps } from './types';
 
 const Input = styled.input<InputProps>(
-  ({ theme, error }) => `
-  font-size: ${theme.fontSizes.S}px;
-  line-height: ${theme.lineHeights.M};
-  width: 100%;
-  padding: 3px ${theme.space.S}px;
-  border-radius: 4px;
-  border: 1px solid ${error ? theme.colors.error : theme.colors.lightgrey};
-  background-color: ${error ? theme.colors.lighterror : 'none'};
-  box-shadow: inset 0 1px 2px rgba(27,31,35,.075);
-`,
+  ({ theme, error }) => css`
+    font-size: ${theme.fontSizes.S}px;
+    line-height: ${theme.lineHeights.M};
+    width: 100%;
+    padding: 3px ${theme.space.S}px;
+    border-radius: 4px;
+    border: 1px solid ${error ? theme.colors.error : theme.colors.lightgrey};
+    background-color: ${error ? theme.colors.lighterror : 'none'};
+    box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075);
+  `,
   compose(space, layout, color, typography, border, shadow),
 );
 
