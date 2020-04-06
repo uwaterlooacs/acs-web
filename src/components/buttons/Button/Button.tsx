dbce515b78d6c68a07e7c95622a8d6a8f13bee9e
Joshua Allum <joshuatallum@gmail.com>
2020-04-06 19:27:21 -0400

    Use new style of styled-components for Button
    - Also lower padding by a little and use theme value

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -1,30 +1,32 @@
-import styled from 'styled-components/macro';
+import styled, { css } from 'styled-components/macro';
 import UnstyledButton from '../UnstyledButton';
 
-export const Button = styled(UnstyledButton)`
-  color: ${({ theme }) => theme.colors.white} !important;
-  text-transform: uppercase;
-  text-decoration: none;
-  background: ${({ theme }) => theme.colors.primary};
-  padding: 20px;
-  border-radius: 5px;
-  display: inline-block;
-  border: none;
-  transition: all 0.4s ease 0s;
-
-  &:hover {
-    letter-spacing: 1px;
-    background: ${({ theme }) => theme.colors.grey};
-    box-shadow: ${({ theme }) => theme.shadows.medium};
+export const Button = styled(UnstyledButton)(
+  ({ theme }) => css`
+    color: ${theme.colors.white} !important;
+    text-transform: uppercase;
+    text-decoration: none;
+    background: ${theme.colors.primary};
+    padding: ${theme.space.M}px;
+    border-radius: 5px;
+    display: inline-block;
+    border: none;
     transition: all 0.4s ease 0s;
-  }
 
-  &:disabled {
-    cursor: not-allowed;
-    letter-spacing: 0;
-    background: ${({ theme }) => theme.colors.lightgrey};
-    box-shadow: none;
-  }
-`;
+    &:hover {
+      letter-spacing: 1px;
+      background: ${theme.colors.grey};
+      box-shadow: ${theme.shadows.medium};
+      transition: all 0.4s ease 0s;
+    }
+
+    &:disabled {
+      cursor: not-allowed;
+      letter-spacing: 0;
+      background: ${theme.colors.lightgrey};
+      box-shadow: none;
+    }
+  `,
+);
 
 export default Button;
