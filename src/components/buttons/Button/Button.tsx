858f34ed24a1ed5af01ab94e6cb5f78b73537ca6
Joshua Allum <joshuatallum@gmail.com>
2020-04-06 04:38:24 -0400

    Update react and other packages
    - Also remove @styled-system/css for now

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -1,41 +1,9 @@
 import styled from 'styled-components/macro';
 import UnstyledButton from '../UnstyledButton';
-import css from '@styled-system/css';
 
 type ButtonProps = {
   variant?: 'primary' | 'secondary';
 };
-export const Button = styled(UnstyledButton)<ButtonProps>(
-  css({
-    borderRadius: 4,
-    border: '1px solid',
-    color: 'primary',
-    p: 2,
-    '&:hover, &:focus': {
-      color: 'white',
-      backgroundColor: 'primary',
-      outline: 0,
-    },
-    '&:active': {
-      boxShadow: 'light',
-    },
-  }),
-  // variant({
-  //   variants: {
-  //     primary: {
-  //       color: 'primary',
-  //       '&:hover, &:focus': {
-  //         bg: 'primary/light',
-  //       },
-  //     },
-  //     secondary: {
-  //       color: 'secondary',
-  //       '&:hover, &:focus': {
-  //         bg: 'secondary/light',
-  //       },
-  //     },
-  //   },
-  // }),
-);
+export const Button = styled(UnstyledButton)<ButtonProps>``;
 
 export default Button;
