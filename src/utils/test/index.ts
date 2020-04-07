900ecde3576561427f7d3289505e1876237fd400
Joshua Allum <joshuatallum@gmail.com>
2020-04-07 00:23:12 -0400

    Export waitFor from utils/test

--- src/utils/test/index.ts
+++ src/utils/test/index.ts
@@ -1,3 +1,3 @@
 export { default as renderHook } from './renderHook';
 export { default as render } from './render';
-export { screen, fireEvent } from '@testing-library/react';
+export { screen, fireEvent, waitFor } from '@testing-library/react';
