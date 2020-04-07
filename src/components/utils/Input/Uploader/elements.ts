cf281b45a18bb6076156b642464e06262fc73e30
Joshua Allum <joshuatallum@gmail.com>
2020-04-07 01:21:08 -0400

    Add small margin to Uploader to match other inputs

--- src/components/utils/Input/Uploader/elements.ts
+++ src/components/utils/Input/Uploader/elements.ts
@@ -4,6 +4,7 @@ import Centered from 'components/utils/Centered';
 export const UploaderContainer = styled(Centered)(
   ({ theme }) => css`
     cursor: pointer;
+    margin: ${theme.space.XS}px;
     border: 4px dashed ${theme.colors.lightgrey};
     border-radius: 16px;
     box-shadow: inset 0 1px 10px rgba(27, 31, 35, 0.075);
