24a5c98d356cb00ee747e4a511050e8146dabec9
Joshua Allum <joshuatallum@gmail.com>
2020-04-07 20:27:07 -0400

    Export Textarea from input index

--- src/components/utils/Input/index.ts
+++ src/components/utils/Input/index.ts
@@ -1,4 +1,5 @@
 export { default as Input } from './Input';
 export { default as Label } from './Label';
 export { default as Select } from './Select';
+export { default as Textarea } from './Textarea';
 export { default as Uploader } from './Uploader';
