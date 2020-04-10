920e1cdaa7d7317af9a3fd5cfb3fd04a017f2e28
Joshua Allum <joshuatallum@gmail.com>
2020-04-10 00:09:50 -0400

    Make logo redirect user to home page

--- src/pages/page/Page.tsx
+++ src/pages/page/Page.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Box from 'components/Box';
 import Centered from 'components/utils/Centered';
+import UnstyledLink from 'components/buttons/UnstyledLink';
 import logo from './acs-logo-color-title.jpg';
 
 const Page: React.FC = ({ children }) => {
   return (
     <Box mt={5} mb={5}>
       <Centered>
-        <img src={logo} alt="UW ACS logo" />
+        <UnstyledLink to="/">
+          <img src={logo} alt="UW ACS logo" />
+        </UnstyledLink>
       </Centered>
       {children}
     </Box>
