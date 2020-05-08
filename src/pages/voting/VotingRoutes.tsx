ce2b887ba1c2fee7b91d61956b6c38d45f12c82b
Joshua Allum <joshuatallum@gmail.com>
2020-05-07 23:01:12 -0400

    Remove vote and submission routes

--- src/pages/voting/VotingRoutes.tsx
+++ src/pages/voting/VotingRoutes.tsx
@@ -3,14 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 const VotingHome = React.lazy(() => import('./VotingHome'));
 const VotingCompleteSignUp = React.lazy(() => import('./VotingCompleteSignUp'));
-const VotingSubmission = React.lazy(() => import('./VotingSubmission'));
-const VotingVote = React.lazy(() => import('./VotingVote'));
 
 const VotingRoutes = () => {
   return (
     <Switch>
-      <Route path="/voting/vote" component={VotingVote} />
-      <Route path="/voting/submission" component={VotingSubmission} />
       <Route path="/voting/completesignup" component={VotingCompleteSignUp} />
       <Route path="/voting" component={VotingHome} />
     </Switch>
