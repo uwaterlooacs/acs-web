a3bb7de0f7b4e3c6198ec115891a1e42ff8ed294
Joshua Allum <joshuatallum@gmail.com>
2020-05-08 00:16:13 -0400

    Upload processFile to directly upload file

--- src/components/utils/Input/Uploader/utils.ts
+++ src/components/utils/Input/Uploader/utils.ts
@@ -1,23 +1,15 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export const processFile = (
+export const processFile = async (
   file: File,
   storage: firebase.storage.Storage,
   storagePath: string,
   { preUpload, postUpload }: Record<string, () => void>,
-) =>
-  new Promise<string>(done => {
-    const reader = new FileReader();
-
-    reader.onload = async () => {
-      preUpload();
-      const binaryStr = reader.result as ArrayBuffer;
-      const ref = storage.ref(storagePath).child(uuidv4());
-      const { ref: resultRef } = await ref.put(binaryStr);
-      const downloadUrl = await resultRef.getDownloadURL();
-      postUpload();
-      done(downloadUrl);
-    };
-
-    reader.readAsArrayBuffer(file);
-  });
+) => {
+  preUpload();
+  const ref = storage.ref(storagePath).child(uuidv4());
+  const { ref: resultRef } = await ref.put(file);
+  const downloadUrl = await resultRef.getDownloadURL();
+  postUpload();
+  return downloadUrl;
+};
